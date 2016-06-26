ebae6545d93681c6d6316ecc4ae1d9f109aa0f50
pjsinco <psinco@gmail.com>
2016-06-26 10:52:38 -0500

    Set up and use inside top-level page template

--- page-toplevel.php
+++ page-toplevel.php
@@ -6,11 +6,47 @@
  *
  * @package omed2016
  */
+?>
 
-get_header();
+<?php get_header(); ?>
 
-get_template_part( 'template-parts/content', 'splash' );
 
+  <?php get_template_part( 'template-parts/content', 'splash' ); ?>
+  
+  <div class="container-fluid wrap content">
+  
+    <!--  Navigation -->
+    <nav class="content__left pagenav__block">
+      <ul class="leftnav__items">
+        <li class="leftnav__item">
+          <a href="">hiya</a>
+        </li>
+    </nav>
+  
+    <!--   Main content -->
+    <section class="content__center">
+
+      <?php 
+        while (have_posts()): the_post();
+
+        get_template_part( 'template-parts/content', 'page' );
+
+        endwhile; 
+      ?>
+    </section> <!-- .col -->
+  
+    <!--   Callouts -->
+    <section class="content__right">
+      <aside class="breakout">
+        <h3>hiya</h3>
+        hello there
+      </aside>
+    </section>
+  
+  </div> <!-- .content -->
+
+
+<?php
 //get_sidebar();
 
 get_footer();
