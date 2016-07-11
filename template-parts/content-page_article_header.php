acbbb7438901da050babfefbf6c204fbf1c9ee24
pjsinco <psinco@gmail.com>
2016-07-11 15:32:23 -0500

    Make lead-in for pages
    Used as a bridge between headline and body copy

--- template-parts/content-page_article_header.php
+++ template-parts/content-page_article_header.php
@@ -8,5 +8,8 @@
 ?>
 	<header class="entry__header">
 		<?php the_title( '<h1 class="entry__title">', '</h1>' ); ?>
+    <?php if ( get_field( 'omed2016_leadin' ) ): ?>
+      <h4 class="leadin"><?php echo get_field( 'omed2016_leadin' ); ?></h4>
+    <?php endif; ?>
 	</header><!-- .entry-header -->
 
