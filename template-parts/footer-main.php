8df1edee35f2cf0efdce8d27c13815d4d374cb13
pjsinco <psinco@gmail.com>
2016-07-20 07:44:13 -0500

    Fix footer links

--- template-parts/footer-main.php
+++ template-parts/footer-main.php
@@ -8,20 +8,20 @@
  ?>
 <div class="footer__block container-fluid">
   <div class="branding__block--footer wrap">
-    <a class="svg" href="http://www.osteopathic.org">
+    <a class="svg" href="http://www.osteopathic.org" target="_blank">
       <div class="icon-aoa" data-grunticon-embed></div>
     </a>
   </div> <!-- .branding__block-footer -->
   <div class="branding__body--footer">
     <p>Serving as the professional family for more than 123,000 osteopathic physicians (DOs) and osteopathic medical students, the American Osteopathic Association (AOA) promotes public health and encourages scientific research.</p>
-    <a href="http://www.osteopathic.org">Learn more</a>
+    <a href=" http://www.doctorsthatdo.org/american-osteopathic-association" target="_blank">Learn more</a>
   </div> <!-- .branding__body-footer -->
 </div> <!-- .footer__block -->
 
 <div class="footer__links container-fluid">
   <ul class="footer__block--row wrap">
-    <li><a href="#">Privacy Policy</a></li>
-    <li><a href="#">Terms of Use</a></li>
-    <li><a href="#">Copyright Notice</a></li>
+    <li><a href="http://www.osteopathic.org/inside-aoa/about/Pages/privacy-policy.aspx" target="_blank">Privacy Policy</a></li>
+    <li><a href="http://www.osteopathic.org/inside-aoa/about/Pages/terms-of-use-agreement.aspx" target="_blank">Terms of Use</a></li>
+    <li><a href="http://www.osteopathic.org/inside-aoa/about/Pages/copyright-notice.aspx" target="_blank">Copyright Notice</a></li>
   </ul>
 </div>
